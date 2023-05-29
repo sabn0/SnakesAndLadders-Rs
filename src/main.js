f2e5d934e9f7dfdca3b913620a73602f42475577
Sabn0 <sabalanoam77@gmail.com>
2023-05-29 12:25:23 +0300

    non working items drawing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 
 const { invoke } = window.__TAURI__.tauri;
+const assets_path = "assets/";
 
-async function build_screen(board_dim, board_length) {
+async function build_board(board_dim, board_length) {
 
   // leave us with empty screen
   document.querySelector("#start_screen").remove();
@@ -29,7 +30,7 @@ async function build_screen(board_dim, board_length) {
   let ctx = canvas.getContext("2d");
   ctx.canvas.width  = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
-  let square_length = board_length / (board_dim);
+  let square_length = board_length / board_dim;
 
   for (var i = 0; i < board_dim; i++) {
       for (var j = 0; j < board_dim; j++) {
@@ -41,31 +42,101 @@ async function build_screen(board_dim, board_length) {
         ctx.fill();
         ctx.strokeStyle = "black";  
         ctx.stroke();
+        let label = (board_dim*board_dim) - (j*board_dim + (board_dim-i));
+        let add_to_pos = square_length / 4;
+        ctx.font = "15px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText(label.toString(), add_to_pos + i*square_length, add_to_pos + j*square_length); 
 
       }
   }
 
 }
 
-async function build_sliders(items) {
-  // 
+async function build_sliders(items, type, board_dim, board_length) {
+
+ // receive an array of ladders or snakes of shape [[start, end], [start, end], ...]
+    // draw the items on the array 
+
+  let square_length = board_length / board_dim;
+  let canvas = document.querySelector("#board_canvas");
+  let asset = assets_path + type + ".png";
+  let ctx = canvas.getContext("2d");
+
+  for (var i = 0; i < items.length; i++) {
+
+    let start = items[i][0];
+    let end = items[i][1];
+    console.log(i);
+
+    // top left x y computation
+    let y_end = board_length - (board_length * (Math.floor(end / board_dim) / board_dim)) - (square_length / 2);
+    let x_end = board_length * ((end % board_dim) / board_dim) + (square_length / 2);
+    let y_start = board_length - (board_length * (Math.floor(start / board_dim) / board_dim)) - (square_length / 2);
+    let x_start = board_length * ((start % board_dim) / board_dim) + (square_length / 2);
+    let top_left_x = Math.min(x_start, x_end);
+    let top_left_y = Math.min(y_start, y_end);
+    let bottom_right_x = Math.max(x_start, x_end);
+    let bottom_right_y = Math.max(y_start, y_end);
+    let x_mid = (bottom_right_x - top_left_x) / 2;
+    let y_mid = (bottom_right_y - top_left_y) / 2;
+
+    // rotation computation
+    let adjacent = bottom_right_y - top_left_y;
+    let opposite = bottom_right_x - top_left_x; 
+    let hypotenuse = Math.sqrt(adjacent*adjacent + opposite*opposite);
+    let angle_radians = Math.acos(adjacent/hypotenuse);
+    let angle_degress = angle_radians * 180 / Math.PI;
+    if (x_start > x_end) {
+      angle_degress = 360 - angle_degress;
+    }
+    let rotation = (angle_degress * Math.PI) / 180;
+
+    // width and height computations
+    let height = hypotenuse;
+    let width = square_length / 2;
+
+    var img = new Image();
+    img.src = asset;
+    
+    img.onload = function () {
+
+      ctx.save();
+
+      ctx.translate( bottom_right_x+ (width / 2), top_left_y + (height / 2));
+      
+      ctx.rotate(rotation);
+
+      ctx.drawImage(img, -width / 2, -height / 2, width, height);
+
+      ctx.restore();
+
+    };
+
+    console.log(start, end, top_left_x, top_left_y, bottom_right_x, bottom_right_y, angle_degress, adjacent, opposite);
+
+  }
+
 }
 
 async function build_players(n) {
   //
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async function () {
 
   container = document.querySelector("#container");
   let board_dim = 10;
   let board_length = 600;
 
-  document.querySelector("#start_button").addEventListener("click", (e) => {
+  document.querySelector("#start_button").addEventListener("click", async function (e) {
+
+    // retrive serialized board from backend
+    let board = await invoke('init_game').then((response) => response ).catch((e) => console.error(e));
 
     e.preventDefault();
-    build_board(board_dim, board_length);
-
+    await build_board(board_dim, board_length);
+    build_sliders(board.ladders, "ladder", board_dim, board_length);
   });
 
 });
